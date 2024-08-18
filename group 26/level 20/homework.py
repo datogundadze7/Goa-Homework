8e8aabd15cb988d431dab2619ce4c3719875c4d3
dato gundadze <davitagundadze32@gmail.com>
2024-08-19 01:26:14 +0400

    Add files via upload

---
+++ group 26/level 20/homework.py
@@ -0,0 +1,69 @@
+#task 1
+a = 5
+b = 7
+
+sum_result = a + b
+print("ჯამი:", sum_result)
+#task 2
+# ორი სტრინგი
+str1 = "გამარჯობა"
+str2 = " მსოფლიო"
+
+# სტრინგების შეერთება (კონკატენაცია)
+result = str1 + str2
+print(result)
+#task 3
+a = 10
+b = 3
+
+# ორ integer-ს შორის განაყოფი
+result = a / b
+print(result)
+#task 4
+print(5 == 5)  # True
+#თანასწორია
+print(5 != 3)  # True
+#არათანასწორი
+print(7 > 3)  # True
+#მეტი
+print(2 < 4)  # True
+#ნაკლები
+print(5 >= 5)  # True
+#მეტი ან ტოლი
+print(3 <= 8)  # True
+#ნაკლები ან ტოლი
+
+#task 5
+print(5 + 5 == 8 + 2)  # True
+
+print(10 - 3 != 4 * 2)  # True
+
+print(3 * 3 > 2 + 4)  # True
+
+print(15 / 3 < 5 + 1)  # False
+
+print(4 ** 2 >= 8 + 8)  # True
+
+print(10 % 3 <= 2)  # True
+
+#task 6
+print(True and True)  # True
+
+print(True and False)  # False
+
+print(False and True)  # False
+
+print(False and False)  # False
+
+print(True or True)  # True
+
+print(True or False)  # True
+
+print(False or True)  # True
+
+print(False or False)  # False
+
+def number_to_string(num):
+    return str(num)
+
+
