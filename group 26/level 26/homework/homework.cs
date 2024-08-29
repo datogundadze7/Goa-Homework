8a5bd18c64040134f5bc7ee0d3665eb7f2d22707
dato gundadze <davitagundadze32@gmail.com>
2024-08-30 02:49:58 +0400

    Add files via upload

---
+++ group 26/level 26/homework/homework.cs
@@ -0,0 +1,45 @@
+/* Back-up fonts */
+body {
+    font-family: 'Roboto', Arial, sans-serif;
+    background-color: #f4f4f4;
+    color: #333;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+    font-family: 'Montserrat', 'Arial Black', sans-serif;
+    color: #222;
+    text-align: center;
+    margin-top: 50px;
+}
+
+h2 {
+    font-family: 'Lora', 'Times New Roman', serif;
+    color: #444;
+    margin-top: 30px;
+    text-align: center;
+}
+
+p {
+    font-family: 'Roboto', Arial, sans-serif;
+    margin: 20px;
+    line-height: 1.6;
+    text-align: justify;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px 0;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px 0;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+}
