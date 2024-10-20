12cf583b4f6bc3bdab4822e898a8f3875e194161
dato gundadze <davitagundadze32@gmail.com>
2024-10-20 13:46:26 +0400

    Delete group 26 directory

--- group 26/level 34/homework.py/homework2.py
+++
@@ -1,8 +0,0 @@
-def sum_mixed_numbers(numbers):
-    # ვცდილობთ თითოეული ელემენტი გარდავქმნათ რიცხვად (თუ შესაძლებელია)
-    clean_numbers = [int(num) for num in numbers if isinstance(num, (int, str)) and str(num).isdigit()]
-    return sum(clean_numbers)
-
-# მაგალითი გამოყენებისათვის
-numbers = [10, "10", "abc", 20, "30", "xyz"]
-print(sum_mixed_numbers(numbers))  # შედეგი: 70
